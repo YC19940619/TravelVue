df81d7d9ae3cd3a4edc842fb3de8ad657e08e025
yangchen <15613592297@163.com>
2018-09-12 16:45:19 +0800

    城市搜索框

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市或拼音"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .search
+    width:100%;
+    height:.72rem;
+    background:$bgColor;
+    box-sizing border-box;
+    padding:0 0.1rem;
+    .search-input
+      box-sizing: border-box;
+      padding:0 0.1rem;
+      width:100%
+      height:.64rem;
+      border-radius:0.04rem;
+      text-align: center;
+      font-size :0.24rem;
+      color: #666
+</style>
