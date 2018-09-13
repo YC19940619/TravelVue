7502aa9217d3ef1145d86b06e68db7897eb2a51b
yangchen <15613592297@163.com>
2018-09-13 11:46:40 +0800

    字母表

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="list">
+      <div class="letter">A</div>
+      <div class="letter">B</div>
+      <div class="letter">C</div>
+      <div class="letter">D</div>
+      <div class="letter">E</div>
+      <div class="letter">F</div>
+      <div class="letter">G</div>
+      <div class="letter">H</div>
+      <div class="letter">I</div>
+      <div class="letter">J</div>
+      <div class="letter">K</div>
+      <div class="letter">L</div>
+      <div class="letter">M</div>
+      <div class="letter">N</div>
+      <div class="letter">O</div>
+      <div class="letter">P</div>
+      <div class="letter">Q</div>
+      <div class="letter">R</div>
+      <div class="letter">S</div>
+      <div class="letter">T</div>
+      <div class="letter">U</div>
+      <div class="letter">V</div>
+      <div class="letter">X</div>
+      <div class="letter">Y</div>
+      <div class="letter">Z</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .list
+    width:.6rem
+    position:absolute
+    right:0
+    top:1.58rem
+    bottom:0
+    font-size :.26rem
+    display:flex
+    flex-direction:column
+    justify-content :center
+    .letter
+      width: 100%
+      padding:.03rem 0
+      color: $bgColor
+      text-align:center
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="list" ref="wapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wapper active">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wapper">
+            <div class="button">西安</div>
+          </div>
+          <div class="button-wapper">
+            <div class="button">三亚</div>
+          </div>
+          <div class="button-wapper">
+            <div class="button">四川</div>
+          </div>
+          <div class="button-wapper">
+            <div class="button">秦皇岛</div>
+          </div>
+          <div class="button-wapper">
+            <div class="button">贵州</div>
+          </div>
+          <div class="button-wapper">
+            <div class="button">长春</div>
+          </div>
+          <div class="button-wapper">
+            <div class="button">五里营</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="city-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bescroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bescroll(this.$refs.wapper)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color:#ccc
+    &:after
+      border-color:#ccc
+  .border-bottom
+    &:before
+      border-color:#ccc
+  .list
+    width:100%;
+    position :absolute;
+    left:0;
+    top:1.58rem;
+    bottom:0;
+    overflow :hidden;
+    .title
+      width: 100%;
+      height:.54rem;
+      line-height :.54rem;
+      background :#eee;
+      font-size :.26rem;
+      text-indent :.2rem
+    .button-list
+      width: 100%;
+      box-sizing :border-box;
+      padding:.1rem
+      padding-right:.6rem
+      overflow:hidden
+      .button-wapper
+        float: left
+        width: 33.33%
+        .button
+          text-align:center
+          font-size :.24rem
+          padding:.1rem 0
+          border:.02rem solid #ccc
+          border-radius:.06rem
+          margin:.1rem
+      .button-wapper.active .button
+        border-color: $bgColor
+        color: $bgColor
+    .city-list
+      width:100%
+      .item
+        width:100%;
+        height:.74rem;
+        line-height:.74rem;
+        font-size :.26rem
+        text-indent :.2rem
+
+</style>
